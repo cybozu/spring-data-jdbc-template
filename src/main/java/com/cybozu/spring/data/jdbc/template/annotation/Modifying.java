61f5a35ff36ec25acd4347d5fdcc2d274c211554
Shumpei Akai <shumpei-akai@cybozu.co.jp>
2017-01-26 16:04:47 +0900

    does not support meta-annotation

--- src/main/java/com/cybozu/spring/data/jdbc/template/annotation/Modifying.java
+++ src/main/java/com/cybozu/spring/data/jdbc/template/annotation/Modifying.java
@@ -6,6 +6,6 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
-@Target({ ElementType.METHOD, ElementType.ANNOTATION_TYPE })
+@Target({ ElementType.METHOD })
 public @interface Modifying {
 }
